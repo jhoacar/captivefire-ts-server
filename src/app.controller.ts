f7f31b1024de77333b2728292461423c6e450f0c
jhoacar <carrerojhoan@gmail.com>
2022-08-05 00:47:02 -0300

    added update handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola';
+  }
+
+  @Post('/openwrt')
+  garantAccessOpenWRT(@Req() request: Request, @Res() response: Response): Response<any, Record<string, any>> {
+    const authorizationHeaders = request.headers.authorization;
+    if (!authorizationHeaders)
+      return response.status(403).json({ error: "Authorization headers missed" });
+    if (!authorizationHeaders.includes('Bearer'))
+      return response.status(403).json({ error: "Bearer missed in header" });
+
+    const token = authorizationHeaders.split('Bearer').pop()?.trim();
+
+    if (!token)
+      return response.status(403).json({ error: "token missed" });
+
+    
+      console.log(token);
+
+    return response.status(202).json({
+      body: 'ok'
+    })
+  }
+
+  @Post('/openwrt/update')
+  updateOpenWRT(@Req() request: Request, @Res() response: Response): Response<any, Record<string, any>>| void{
+    const authorizationHeaders = request.headers.authorization;
+    if (!authorizationHeaders)
+      return response.status(403).json({ error: "Authorization headers missed" });
+    if (!authorizationHeaders.includes('Bearer'))
+      return response.status(403).json({ error: "Bearer missed in header" });
+
+    const token = authorizationHeaders.split('Bearer').pop()?.trim();
+
+    if (!token)
+      return response.status(403).json({ error: "token missed" });
+
+    const pathPhar = require("path").resolve(__dirname + '/../../captivefire-openwrt-server/build/index.phar');
+    response.status(202).sendFile(pathPhar);
   }
 }
